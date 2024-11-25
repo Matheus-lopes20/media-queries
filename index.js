2d16ad92efa58a1f787808bd486be93a4e8186f7
Matheus <matheuslopes.cocos@gmail.com>
2024-11-25 09:41:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function clickMenu() {
+    if (item.style.display == 'block') {
+        item.style.display = 'none'
+    } else {
+        item.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        item.style.display = 'block'
+    } else {
+        item.style.display = 'none'
+    }
+}
